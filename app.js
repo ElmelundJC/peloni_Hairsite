e35bc2fc6da96dc779154d82048d28403f4cd066
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-11-08 13:25:07 +0100

    sever setup

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const hello = 'hello';
+const express = require("express");
+const fs = require("fs");
 
-const sovs = "kartofler";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+const frontpage = fs.readFileSync(__dirname + "/public/frontpage.html", "utf-8");
+
+app.get("/", (req, res) => {
+    res.send(frontpage);
+});
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Could not connect to server" + error);
+    };
+    console.log(`Connected to server on port ${port}`);
+});
