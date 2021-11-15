ceeffd4b26f3c200b0b87018709103415a22d50d
ElmelundJC <christianelmelund@hotmail.com>
2021-11-15 14:51:31 +0100

    Routes - Booking, news

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ app.use(express.static(__dirname + "/public"));
 
 const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
 
+const newspage = fs.readFileSync(__dirname + "/public/newspage/newspage.html", "utf-8");
+const bookingpage = fs.readFileSync(__dirname + "/public/bookingspage/bookingspage.html", "utf-8");
+
+
+
 app.get("/", (req, res) => {
     res.send(frontpage);
 });
+
+app.get("/nyheder", (req, res) => {
+    res.send(newspage);
+});
+
+app.get("/booking", (req, res) => {
+    res.send(bookingpage);
+});
+
 
 app.listen(port, (error) => {
     if (error) {
