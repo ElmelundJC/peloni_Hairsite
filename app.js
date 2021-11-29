be1314a4aca7dbd9764e17952950dc094f5818e3
ElmelundJC <christianelmelund@hotmail.com>
2021-11-29 12:07:59 +0100

    POST route -  Users implemented

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const express = require("express");
 const fs = require("fs");
 require('./database/db');
 const User = require('./models/userModel');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
+// Middleware
+
 app.use(express.static(__dirname + "/public"));
 
+// body parser
+app.use(bodyParser.json());
+
+
+
+// // error handling middleware
+// app.use((err, req, res, next) => {
+//     //console.log(err);
+//     res.status(422).send({ error: err.message });
+// });
+
+
+// Pages
 const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
 const about = fs.readFileSync(__dirname + "/public/about/about.html", "utf-8");
 const services = fs.readFileSync(__dirname + "/public/services/services.html", "utf-8");
@@ -43,21 +61,17 @@ app.get("/booking", (req, res) => {
     res.send(bookingpage);
 });
 
-// app.post("/signup", async (req, res) => {
-//     try {
-//         const user = await User.create({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: req.body.password,
-//             age: req.body.age
-//         });
+app.post("/signup", async (req, res) => {
 
-//         console.log(user);
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// });
+    const user = new User(req.body)
+    try {
+        await user.save()
+        console.log(user);
+        res.status(201)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+});
 
 // app.use('/', browsingRoute);
 
