e34e07dd68b71b4110d1ce50c54c8cc9fceb9c0f
ElmelundJC <christianelmelund@hotmail.com>
2021-12-21 20:22:49 +0100

    minor modification

--- routes2/userRoutes.js
+++ routes2/userRoutes.js
@@ -4,16 +4,6 @@ const jwt = require('jsonwebtoken');
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
-
-const getAllEvents = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-};
-
-
-
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -26,8 +16,7 @@ router.get('/getUser/:id', authController.protect, userController.getUser);
 
 router
     .route('/')
-    .get(authController.protect, userController.getAllUsers)
-    .get(getAllEvents);
+    .get(authController.protect, userController.getAllUsers);
 
 
 router
